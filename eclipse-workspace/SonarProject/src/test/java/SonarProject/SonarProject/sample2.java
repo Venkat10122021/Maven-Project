51237f88c19aa668be8d17fd1d7d1afdb5980065
Venkat10122021 <venkateshpedharapu@gmail.com>
2021-07-18 13:11:57 +0530

    first commit

---
+++ eclipse-workspace/SonarProject/src/test/java/SonarProject/SonarProject/sample2.java
@@ -0,0 +1,20 @@
+package SonarProject.SonarProject;
+
+public class sample2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 1000,b=9;
+		int c;
+		
+		c= a+b;
+		
+
+		
+		System.out.println(c);
+		
+	}
+
+}
